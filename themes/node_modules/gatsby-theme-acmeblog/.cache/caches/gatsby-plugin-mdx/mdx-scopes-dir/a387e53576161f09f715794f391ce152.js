f4d8ad6403db1a90ed4c39d0b5fd9f26bb7acc8a
Tiago Sanchez <tiagofsanchez@gmail.com>
2020-05-25 14:41:28 +0800

    init

--- themes/node_modules/gatsby-theme-acmeblog/.cache/caches/gatsby-plugin-mdx/mdx-scopes-dir/a387e53576161f09f715794f391ce152.js
+++
@@ -1,6 +0,0 @@
-import Wave from "..\\..\\..\\..\\src\\components\\wave";
-import * as React from 'react';
-export default {
-  Wave,
-  React
-};
--- themes/node_modules/gatsby-theme-acmeblog/src/pages/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'; 
-
-const IndexPage = () => { 
-    return <div>Whatever you want it to be</div>
-}
-
-export default IndexPage;
