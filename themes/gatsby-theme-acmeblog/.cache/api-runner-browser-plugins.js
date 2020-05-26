6b1281c241efd9a121d4b711e90dc66c6d7b2112
Tiago Sanchez <tiagofsanchez@gmail.com>
2020-05-26 23:09:40 +0800

    chore: fixing the commits and gitignore

--- themes/gatsby-theme-acmeblog/.cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('../../node_modules/gatsby-remark-images/gatsby-browser.js'),
-      options: {"plugins":[],"maxWidth":800},
-    },{
-      plugin: require('../../node_modules/gatsby-plugin-mdx/gatsby-browser.js'),
-      options: {"plugins":[],"extensions":[".mdx",".md"],"gatsbyRemarkPlugins":[{"resolve":"gatsby-remark-images","options":{"maxWidth":1200}}]},
-    }]
