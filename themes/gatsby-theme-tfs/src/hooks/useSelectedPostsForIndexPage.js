082b73a5a2e64323a2c2ec964fec2a7b64e09366
Tiago Sanchez <tiagofsanchez@gmail.com>
2020-07-02 13:50:42 +0800

    feat(selected posts on landing page)

---
+++ themes/gatsby-theme-tfs/src/hooks/useSelectedPostsForIndexPage.js
@@ -0,0 +1,52 @@
+import { useStaticQuery, graphql } from "gatsby";
+
+const useSelectedPostsForIndexPage = () => {
+  const data = useStaticQuery(graphql`
+    {
+      posts: allMdx(
+        filter: { frontmatter: { date: { ne: null }, selected: { eq: "yes" } } }
+        limit: 5
+        sort: { order: DESC, fields: frontmatter___date }
+      ) {
+        nodes {
+          timeToRead
+          frontmatter {
+            title
+            date(formatString: "MMMM DD, YYYY")
+            category
+            tags
+            thumbnail {
+              childImageSharp {
+                fluid(maxWidth: 2000, quality: 100) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+          fields {
+            slug
+          }
+          excerpt
+        }
+      }
+    }
+  `);
+  let selectedPostsArray = [];
+  data.posts.nodes.map((post) => {
+    const postObject = {
+      title: post.frontmatter.title,
+      date: post.frontmatter.date,
+      category: post.frontmatter.category,
+      tags: post.frontmatter.tags,
+      excerpt: post.excerpt,
+      slug: post.fields.slug,
+      timeToRead: post.timeToRead,
+      thumbnail: post.frontmatter.thumbnail,
+    };
+    selectedPostsArray.push(postObject);
+    return selectedPostsArray;
+  });
+  return selectedPostsArray;
+};
+
+export default useSelectedPostsForIndexPage;
