c7c8c7d1f6b3080bb7f8e4529b7f9c16ac1575e3
Tiago Sanchez <tiagofsanchez@gmail.com>
2020-07-06 16:34:48 +0800

    feature: next and previous post nav implemented

---
+++ themes/gatsby-theme-tfs/src/components/shared/avatar.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import Img from "gatsby-image";
+import styled from "@emotion/styled";
+import PropTypes from "prop-types";
+
+const ImageWrapper = styled.div`
+  width: ${(props) => props.width};
+  border-radius: 50%;
+`;
+
+const Avatar = ({ width }) => {
+  const data = useStaticQuery(graphql`
+    {
+      logo: file(relativePath: { eq: "logo.png" }) {
+        childImageSharp {
+          fluid(grayscale: true) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `);
+  return data.logo !== null ? (
+    <ImageWrapper width={width}>
+      <Img fluid={data.logo.childImageSharp.fluid} />
+    </ImageWrapper>
+  ) : null;
+};
+
+Avatar.propTypes = {
+  width: PropTypes.string,
+};
+
+export default Avatar;
