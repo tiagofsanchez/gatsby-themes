286b576d8f4046b1799b80c5455087cdbb6c5a45
Tiago Sanchez <tiagofsanchez@gmail.com>
2020-08-11 22:56:51 +0800

    chore: improve the 404 page

--- themes/gatsby-theme-tfs/src/pages/404.js
+++ themes/gatsby-theme-tfs/src/pages/404.js
@@ -1,10 +1,34 @@
 /** @jsx jsx */
 import { jsx } from "theme-ui";
+import { useStaticQuery, graphql } from "gatsby";
+import Img from "gatsby-image";
+import styled from "@emotion/styled";
 
-const NoPage = () => (
-  <div sx={{ variant: `layout.container` }}>
-    <h1>Ups...something went wrong! This page doesn't exist</h1>
-  </div>
-);
+const ImageWrapper = styled.div`
+  width: 500px;
+  margin: auto;
+`;
+
+const NoPage = () => {
+  const data = useStaticQuery(graphql`
+  { 
+    img: file(relativePath: {eq: "404.png"}) {
+      childImageSharp {
+        fluid(grayscale: true) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+  `);
+  return (
+    <div sx={{ variant: `layout.container`, textAlign: `center`, margin: `auto` }}>
+      <h1>Ups...something went wrong!</h1>
+      <ImageWrapper>
+        <Img fluid={data.img.childImageSharp.fluid}/>
+      </ImageWrapper>
+    </div>
+  );
+};
 
 export default NoPage;
