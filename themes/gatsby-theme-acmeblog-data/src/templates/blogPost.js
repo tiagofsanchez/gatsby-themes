f4a6c0b1271995749cb3dbb066e4f5f2a960a743
Tiago Sanchez <tiagofsanchez@gmail.com>
2020-08-11 15:08:46 +0800

    chore: implemented gatsby-remark-reading-time with the rigth reading time and word count in the gatsby-theme-acmeblog-data

--- themes/gatsby-theme-acmeblog-data/src/templates/blogPost.js
+++ themes/gatsby-theme-acmeblog-data/src/templates/blogPost.js
@@ -55,6 +55,12 @@ export const blogPost = graphql`
         }
         fields {
           slug
+          readingTime {
+            words
+            time
+            minutes
+            text
+          }
         }
       }
     }
