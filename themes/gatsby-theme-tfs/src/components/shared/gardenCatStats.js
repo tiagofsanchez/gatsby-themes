1af13f747f74c04bd329552de150c61e9456c869
Tiago Sanchez <tiagofsanchez@gmail.com>
2020-08-19 15:23:46 +0800

    styling: cat pie chart

--- themes/gatsby-theme-tfs/src/components/shared/gardenCatStats.js
+++ themes/gatsby-theme-tfs/src/components/shared/gardenCatStats.js
@@ -23,21 +23,51 @@ const Flex = styled.div`
 const COLORS = ["#d23669", "#DB5E87", "#E486A5", "#EDAEC3"];
 const _ = require("lodash");
 
+const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({
+  cx,
+  cy,
+  midAngle,
+  innerRadius,
+  outerRadius,
+  percent,
+  index,
+}) => {
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.2;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+  return (
+    <text
+      x={x}
+      y={y}
+      fill="white"
+      textAnchor={x > cx ? "start" : "end"}
+      dominantBaseline="central"
+    >
+      {`${(percent * 100).toFixed(0)}%`}
+    </text>
+  );
+};
+
 const GardenCatStats = ({ gardenCatStats }) => {
   const { blogPath, categoryPath } = useAcmeBlogConfig();
   return (
     <GridContainer sx={{ bg: `hover` }}>
       <h4 sx={{ m: `0` }}>Categories</h4>
-      <PieChart width={160} height={215} sx={{ margin: `auto` }}>
+      <PieChart width={190} height={215} sx={{ margin: `auto` }}>
         <Pie
           data={gardenCatStats}
           dataKey="value"
           cx="50%"
           cy="50%"
           innerRadius={50}
-          outerRadius={80}
+          outerRadius={90}
           paddingAngle={4}
           fill="#8884d8"
+          label={renderCustomizedLabel}
+          labelLine={false}
+          animationDuration="1000"
         >
           {gardenCatStats.map((entry, index) => (
             <Cell
